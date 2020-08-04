f74a3677be02229ed3775c26405f9d00b6a64807
jcgsr <cabralguerra@gmail.com>
2020-08-04 11:28:53 -0300

    cambio alert

--- src/components/cambio/Conversor.vue
+++ src/components/cambio/Conversor.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="conversor">
-    <div class="card border-secondary mb-3">
-      <div class="card-body">
-        <h4 class="card-title">{{ moedaA }} para {{ moedaB }}</h4>
-        <input class="form-control" type="number" v-model="moedaA_value" :placeholder="moedaA" />
-        <button class="btn btn-secondary" @click="converter">Converter</button>
-        <h2 id="value">{{ moedaB_value | currency }}</h2>
+  <div class="container">
+    <div class="alert alert-dismissible alert-danger" v-if="errored">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <p>Erro! O servidor não está respondendo.</p>
+    </div>
+    <div class="conversor">
+      <div class="card border-secondary mb-3">
+        <div class="card-body">
+          <h4 class="card-title">{{ moedaA }} para {{ moedaB }}</h4>
+          <input class="form-control" type="number" v-model="moedaA_value" :placeholder="moedaA" />
+          <button class="btn btn-secondary" @click="converter">Converter</button>
+          <h2 id="value">{{ moedaB_value | currency }}</h2>
+        </div>
       </div>
-    </div>    
+    </div>
   </div>
 </template>
 
@@ -18,7 +24,8 @@ export default {
   data() {
     return {
       moedaA_value: "",
-      moedaB_value: 0,
+      moedaB_value: 0,      
+      errored: false,
     };
   },
 
@@ -39,7 +46,12 @@ export default {
           this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(
             2
           );
-        });
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
   },
   filters: {
@@ -61,5 +73,10 @@ export default {
 
 #value {
   margin-top: 1rem;
+}
+
+.alert button {
+  max-width: 10%;
+  display: block;
 }
 </style>
--- src/components/cambio/ConversorEuro.vue
+++ src/components/cambio/ConversorEuro.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="conversor">
-    <div class="card border-secondary mb-3">
-      <div class="card-body">
-        <h4 class="card-title">{{ moedaA }} para {{ moedaB }}</h4>
-        <input class="form-control" type="number" v-model="moedaA_value" :placeholder="moedaA" />
-        <button class="btn btn-secondary" @click="converter">Converter</button>
-        <h2 id="value">{{ moedaB_value | currency }}</h2>
+  <div class="container">
+    <div class="alert alert-dismissible alert-danger" v-if="errored">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <p>Erro! O servidor não está respondendo.</p>
+    </div>
+    <div class="conversor">
+      <div class="card border-secondary mb-3">
+        <div class="card-body">
+          <h4 class="card-title">{{ moedaA }} para {{ moedaB }}</h4>
+          <input class="form-control" type="number" v-model="moedaA_value" :placeholder="moedaA" />
+          <button class="btn btn-secondary" @click="converter">Converter</button>
+          <h2 id="value">{{ moedaB_value | currency }}</h2>
+        </div>
       </div>
-    </div>    
+    </div>
   </div>
 </template>
 
@@ -19,6 +25,7 @@ export default {
     return {
       moedaA_value: "",
       moedaB_value: 0,
+      errored: false,
     };
   },
   methods: {
@@ -38,7 +45,12 @@ export default {
           this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(
             2
           );
-        });
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
     },
   },
   filters: {
