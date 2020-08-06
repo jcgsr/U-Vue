4fa3eebbc00f3f1ec7738f9654bffe016243cd97
jcgsr <cabralguerra@gmail.com>
2020-08-06 10:03:25 -0300

    imc table

--- src/components/IMC.vue
+++ src/components/IMC.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="imc">
-  
-      <h2>IMC</h2>
-  <div class="card border-secondary mb-3">
-    <div class="card-body">
-		Peso
-      <input
-        class="form-control"
-        @focus="$event.target.select()"
-        type="number"
-        name="peso"
-        v-model="peso"
-      /> Altura
-      <input
-        class="form-control"
-        @focus="$event.target.select()"
-        type="number"
-        name="altura"
-        v-model="altura"
-      />
-      <button class="animate__animated animate__tada btn btn-secondary" @click="zerar">zerar</button>
-      <h2>{{ result }}</h2>
+    <h2>IMC</h2>
+    <div class="card border-secondary mb-3">
+      <div class="card-body">
+        Peso
+        <input
+          class="form-control"
+          @focus="$event.target.select()"
+          type="number"
+          name="peso"
+          v-model="peso"
+        /> Altura
+        <input
+          class="form-control"
+          @focus="$event.target.select()"
+          type="number"
+          name="altura"
+          v-model="altura"
+        />
+        <button class="animate__animated animate__tada btn btn-secondary" @click="zerar">zerar</button>
+        <h2>{{ result }}</h2>
+      </div>
     </div>
-  </div>
+    <div class="tabela">
+      <table border="3">
+        <thead>
+          <tr class="table-active">
+            <th scope="col">IMC</th>
+            <th scope="col">Situação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Abaixo de 16</td>
+            <td>Muito abaixo do peso</td>
+          </tr>
+          <tr>
+            <td>Entre 16 e 19</td>
+            <td>Abaixo do peso</td>
+          </tr>
+          <tr>
+            <td>Entre 19 e 25</td>
+            <td>Peso normal</td>
+          </tr>
+          <tr>
+            <td>Entre 25 e 30</td>
+            <td>Excesso de peso</td>
+          </tr>
+          <tr>
+            <td>Entre 30 e 35</td>
+            <td>Obesidade</td>
+          </tr>
+          <tr>
+            <td>Acima de 35</td>
+            <td>Obesidade mórbida</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -36,7 +71,10 @@ export default {
   },
   computed: {
     result() {
-      return (parseFloat(this.peso) / (parseFloat(this.altura) * parseFloat(this.altura))).toFixed(2);
+      return (
+        parseFloat(this.peso) /
+        (parseFloat(this.altura) * parseFloat(this.altura))
+      ).toFixed(2);
     },
   },
   methods: {
@@ -55,13 +93,34 @@ export default {
   color: rgba(245, 245, 7, 0.479);
 }
 
-.card button {
-   margin-top: 1rem;
+.tabela {
+  width: 30%;
 }
 
-  @media only screen and (max-width: 600px) {
-.card {
-		max-width: 80%;	
-		}
+.tabela td,
+tr,
+th {
+  font-weight: bold;
+  padding: 1rem;
+  font-size: 1.5rem;
+}
+
+.card button {
+  margin-top: 1rem;
+}
+
+@media only screen and (max-width: 600px) {
+  .card {
+    max-width: 80%;
   }
+  .tabela td,
+  tr,
+  th {
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+  .tabela {
+    width: 80%;
+  }
+}
 </style>
